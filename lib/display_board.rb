47cfdf7fb8547567b0689caa5fe3e6fad9cb123d
Heath Mast <hrmast@gmail.com>
2020-03-08 05:54:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
+def display_board(board)
+    puts = "Welcome to Tic Tac Toe"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+end
 # out the current state.
